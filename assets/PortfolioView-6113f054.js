edac8a11f6dc5943e7cb886aa88d96830c477fe6
snufera <netostt91@gmail.com>
2023-05-22 17:15:32 -0300

    more fix

---
+++ assets/PortfolioView-6113f054.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c}from"./index-1d83ced9.js";const r={};function t(n,s){return e(),c("h1",null,"Portfolio")}const a=o(r,[["render",t]]);export{a as default};
--- assets/PortfolioView-c6195c39.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as e,c}from"./index-8a90d411.js";const r={};function t(n,s){return e(),c("h1",null,"Portfolio")}const a=o(r,[["render",t]]);export{a as default};
